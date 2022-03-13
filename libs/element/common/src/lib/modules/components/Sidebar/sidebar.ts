da9145551bb4f2832fc46da216c24a8c1af15b64
Steven Taniardi <steven.6880@ski.sch.id>
2022-03-13 16:24:03 +0700

    ketinggalan

---
+++ libs/element/common/src/lib/modules/components/Sidebar/sidebar.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+    selector: 'ubud-element-common-sidebar-component',
+    templateUrl: './sidebar.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Sidebar {
+}
